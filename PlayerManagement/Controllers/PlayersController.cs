54756e5ec6639bd53468da0b37c4e7973fe0071e
NicolasKeidong <122652469+NicolasKeidong@users.noreply.github.com>
2023-07-07 16:07:26 -0400

    preparation for Excel report

--- PlayerManagement/Controllers/PlayersController.cs
+++ PlayerManagement/Controllers/PlayersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
@@ -7,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
+using OfficeOpenXml.Style;
+using OfficeOpenXml;
 using PlayerManagement.CustomControllers;
 using PlayerManagement.Data;
 using PlayerManagement.Models;
@@ -521,6 +524,142 @@ namespace PlayerManagement.Controllers
                     }
                 }
             }
+        }
+
+        public IActionResult DownloadPlayers()
+        {
+            //Get the players
+            var plyrs = from p in _context.Players
+                        .Include(p => p.PlayerPosition)
+                        .Include(p => p.Plays).ThenInclude(p => p.PlayerPosition)
+                        .Include(p => p.Team)
+                        orderby p.FirstName descending
+                        select new
+                        {
+                           FirstName = p.FirstName,
+                           LastName = p.LastName,
+                           DOB = p.DOB,
+                           PlayerPosition = p.PlayerPosition.PlayerPos
+                        };
+            //How many rows?
+            int numRows = plyrs.Count();
+
+            if (numRows > 0) //We have data
+            {
+                //Create a new spreadsheet from scratch.
+                using (ExcelPackage excel = new ExcelPackage())
+                {
+
+                    //Note: you can also pull a spreadsheet out of the database if you
+                    //have saved it in the normal way we do, as a Byte Array in a Model
+                    //such as the UploadedFile class.
+                    //
+                    // Suppose...
+                    //
+                    // var theSpreadsheet = _context.UploadedFiles.Include(f => f.FileContent).Where(f => f.ID == id).SingleOrDefault();
+                    //
+                    //    //Pass the Byte[] FileContent to a MemoryStream
+                    //
+                    // using (MemoryStream memStream = new MemoryStream(theSpreadsheet.FileContent.Content))
+                    // {
+                    //     ExcelPackage package = new ExcelPackage(memStream);
+                    // }
+
+                    var workSheet = excel.Workbook.Worksheets.Add("Appointments");
+
+                    //Note: Cells[row, column]
+                    workSheet.Cells[3, 1].LoadFromCollection(plyrs, true);
+
+                    //Style first column for dates
+                    workSheet.Column(1).Style.Numberformat.Format = "yyyy-mm-dd";
+
+                    //Style fee column for currency
+                    workSheet.Column(4).Style.Numberformat.Format = "###,##0.00";
+
+                    //Note: You can define a BLOCK of cells: Cells[startRow, startColumn, endRow, endColumn]
+                    //Make Date and Patient Bold
+                    workSheet.Cells[4, 1, numRows + 3, 2].Style.Font.Bold = true;
+
+                    //Note: these are fine if you are only 'doing' one thing to the range of cells.
+                    //Otherwise you should USE a range object for efficiency
+                    using (ExcelRange totalfees = workSheet.Cells[numRows + 4, 4])//
+                    {
+                        totalfees.Formula = "Sum(" + workSheet.Cells[4, 4].Address + ":" + workSheet.Cells[numRows + 3, 4].Address + ")";
+                        totalfees.Style.Font.Bold = true;
+                        totalfees.Style.Numberformat.Format = "$###,##0.00";
+                    }
+
+                    //Set Style and backgound colour of headings
+                    using (ExcelRange headings = workSheet.Cells[3, 1, 3, 6])
+                    {
+                        headings.Style.Font.Bold = true;
+                        var fill = headings.Style.Fill;
+                        fill.PatternType = ExcelFillStyle.Solid;
+                        fill.BackgroundColor.SetColor(Color.LightBlue);
+                    }
+
+                    ////Boy those notes are BIG!
+                    ////Lets put them in comments instead.
+                    //for (int i = 4; i < numRows + 4; i++)
+                    //{
+                    //    using (ExcelRange Rng = workSheet.Cells[i, 7])
+                    //    {
+                    //        string[] commentWords = Rng.Value.ToString().Split(' ');
+                    //        Rng.Value = commentWords[0] + "...";
+                    //        //This LINQ adds a newline every 7 words
+                    //        string comment = string.Join(Environment.NewLine, commentWords
+                    //            .Select((word, index) => new { word, index })
+                    //            .GroupBy(x => x.index / 7)
+                    //            .Select(grp => string.Join(" ", grp.Select(x => x.word))));
+                    //        ExcelComment cmd = Rng.AddComment(comment, "Apt. Notes");
+                    //        cmd.AutoFit = true;
+                    //    }
+                    //}
+
+                    //Autofit columns
+                    workSheet.Cells.AutoFitColumns();
+                    //Note: You can manually set width of columns as well
+                    //workSheet.Column(7).Width = 10;
+
+                    //Add a title and timestamp at the top of the report
+                    workSheet.Cells[1, 1].Value = "Appointment Report";
+                    using (ExcelRange Rng = workSheet.Cells[1, 1, 1, 6])
+                    {
+                        Rng.Merge = true; //Merge columns start and end range
+                        Rng.Style.Font.Bold = true; //Font should be bold
+                        Rng.Style.Font.Size = 18;
+                        Rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    }
+                    //Since the time zone where the server is running can be different, adjust to 
+                    //Local for us.
+                    DateTime utcDate = DateTime.UtcNow;
+                    TimeZoneInfo esTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+                    DateTime localDate = TimeZoneInfo.ConvertTimeFromUtc(utcDate, esTimeZone);
+                    using (ExcelRange Rng = workSheet.Cells[2, 6])
+                    {
+                        Rng.Value = "Created: " + localDate.ToShortTimeString() + " on " +
+                            localDate.ToShortDateString();
+                        Rng.Style.Font.Bold = true; //Font should be bold
+                        Rng.Style.Font.Size = 12;
+                        Rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                    }
+
+                    //Ok, time to download the Excel
+
+                    try
+                    {
+                        Byte[] theData = excel.GetAsByteArray();
+                        string filename = "Appointments.xlsx";
+                        string mimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                        return File(theData, mimeType, filename);
+                    }
+                    catch (Exception)
+                    {
+                        return BadRequest("Could not build and download the file.");
+                    }
+                }
+            }
+            return NotFound("No data.");
         }
 
         //Not needed since we are in a CognizantController
