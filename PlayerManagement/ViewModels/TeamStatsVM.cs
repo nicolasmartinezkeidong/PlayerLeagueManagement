8b7c336aa57e3a520d2d6998d43152eb5304f212
NicolasKeidong <nicolasmartinezkeidong@outlook.com>
2023-07-17 23:49:01 -0400

    wip - The method need to be fixed in Controller Player

---
+++ PlayerManagement/ViewModels/TeamStatsVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlayerManagement.ViewModels
+{
+    public class TeamStatsVM
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Team")]
+        public string TeamName { get; set; }
+
+        public int Goals { get; set; }
+
+        public int RedCards { get; set; }
+
+        public int YellowCards { get; set; }
+    }
+}
