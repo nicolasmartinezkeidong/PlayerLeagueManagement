e5fce3f65e883fe84e9d82e195df6a2662ca4827
NicolasKeidong <nicolasmartinezkeidong@outlook.com>
2023-07-19 22:48:30 -0400

    DB Initializer for roles

---
+++ PlayerManagement/Data/ApplicationDbInitializer.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace PlayerManagement.Data
+{
+    public static class ApplicationDbInitializer
+    {
+        public static async void Seed(IApplicationBuilder applicationBuilder)
+        {
+            ApplicationDbContext context = applicationBuilder.ApplicationServices.CreateScope()
+                .ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            try
+            {
+                ////Delete the database if you need to apply a new Migration
+                //context.Database.EnsureDeleted();
+                //Create the database if it does not exist and apply the Migration
+                context.Database.Migrate();
+
+                //Create Roles
+                var RoleManager = applicationBuilder.ApplicationServices.CreateScope()
+                    .ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                string[] roleNames = { "Admin", "Capitain" };
+                IdentityResult roleResult;
+                foreach (var roleName in roleNames)
+                {
+                    var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                    if (!roleExist)
+                    {
+                        roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                    }
+                }
+                //Create Users
+                var userManager = applicationBuilder.ApplicationServices.CreateScope()
+                    .ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                if (userManager.FindByEmailAsync("admin1@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "admin1@outlook.com",
+                        Email = "admin1@outlook.com"
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "password").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Admin").Wait();
+                    }
+                }
+                if (userManager.FindByEmailAsync("super1@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "capatain1@outlook.com",
+                        Email = "capatain1@outlook.com"
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "password").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Capitain").Wait();
+                    }
+                }
+                if (userManager.FindByEmailAsync("user1@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "user1@outlook.com",
+                        Email = "user1@outlook.com"
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "password").Result;
+                    //Not in any role
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.GetBaseException().Message);
+            }
+        }
+    }
+
+}
