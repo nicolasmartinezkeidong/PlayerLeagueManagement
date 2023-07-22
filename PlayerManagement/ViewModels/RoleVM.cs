e66d75f6569eb7ac57ca3568f205132ed689ec8f
NicolasKeidong <nicolasmartinezkeidong@outlook.com>
2023-07-21 23:43:54 -0400

    User roles is working, todo typos and more roles

---
+++ PlayerManagement/ViewModels/RoleVM.cs
@@ -0,0 +1,14 @@
+﻿namespace PlayerManagement.ViewModels
+{
+    /// <summary>
+    /// Class to be used as a 'checkbox' to select roles
+    /// </summary>
+    public class RoleVM
+    {
+        public string RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public bool Assigned { get; set; }
+    }
+}
