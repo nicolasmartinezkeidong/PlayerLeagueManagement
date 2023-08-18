342f9b6f823dd4acb900bfdafec1cb289e63185d
NicolasKeidong <nicolasmartinezkeidong@outlook.com>
2023-08-17 23:47:12 -0400

    Implemented classes and interfaces for email services

---
+++ PlayerManagement/Utilities/IMyEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using PlayerManagement.ViewModels;
+
+namespace PlayerManagement.Utilities
+{
+    public interface IMyEmailSender
+    {
+        Task SendOneAsync(string name, string email, string subject, string htmlMessage);
+        Task SendToManyAsync(EmailMessage emailMessage);
+    }
+}
