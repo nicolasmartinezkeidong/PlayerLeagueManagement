ca18fad4bbad94a256ba189a2dc9a79451e7084f
NicolasKeidong <nicolasmartinezkeidong@outlook.com>
2023-09-11 23:07:19 -0400

    preparing code to add table/standings to the home page

---
+++ PlayerManagement/ViewModels/StandingsVM.cs
@@ -0,0 +1,17 @@
+﻿namespace PlayerManagement.ViewModels
+{
+    public class StandingsVM
+    {
+        public int Position { get; set; }
+        public string TeamName { get; set; }
+        public int Played { get; set; }
+        public int Won { get; set; }
+        public int Drawn { get; set; }
+        public int Lost { get; set; }
+        public int GoalsFavor { get; set; }
+        public int GoalsAgainst { get; set; }
+        public int GoalsDifference { get; set; }
+        public int Points { get; set; }
+        public string Form { get; set; }
+    }
+}
