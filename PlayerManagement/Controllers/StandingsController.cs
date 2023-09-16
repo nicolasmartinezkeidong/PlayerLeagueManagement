5965a43a1720939f112f3ab3b4fdee4f7448ce1e
NicolasKeidong <nicolasmartinezkeidong@outlook.com>
2023-09-15 23:33:25 -0400

    wip stanginds controller

--- PlayerManagement/Controllers/StandingsController.cs
+++ PlayerManagement/Controllers/StandingsController.cs
@@ -19,45 +19,49 @@ namespace PlayerManagement.Controllers
         // GET: Standings
         public async Task<IActionResult> Index()
         {
-            var standings = await (from s in _context.StandingsVM
-                               .OrderBy(s => s.Position)
-                                   select new StandingsVM
-                                   {
-                                       Id = s.Id,
-                                       Position = s.Position,
-                                       TeamName = s.TeamName,
-                                       Played = s.Played,
-                                       Won = s.Won,
-                                       Drawn = s.Drawn,
-                                       Lost = s.Lost,
-                                       GoalsFavor = s.GoalsFavor,
-                                       GoalsAgainst = s.GoalsAgainst,
-                                       GoalsDifference = s.GoalsDifference,
-                                       Points = s.Points,
-                                       Form = s.Form
-                                   }).ToListAsync();
+            var standings = await _context.StandingsVM
+                .OrderBy(s => s.Position)
+                .ToListAsync();
 
-            //// Debugging output
-            //foreach (var item in standings)
-            //{
-            //    Console.WriteLine($"Team: {item.TeamName}, Points: {item.Points}");
-            //}
+            // matches results
+            var matches = await _context.MatchSchedules.ToListAsync();
+
+            // Calculate team statistics and update the standings
+            foreach (var standing in standings)
+            {
+                var teamName = standing.TeamName;
+
+                // Filter matches for the current team, both home and away
+                var teamMatches = matches.Where(match =>match.HomeTeam.Name == teamName || match.AwayTeam.Name == teamName);
+
+                standing.Played = teamMatches.Count();
+                standing.Won = teamMatches.Count(match =>(match.HomeTeam.Name == teamName && match.HomeTeamScore > match.AwayTeamScore) || (match.AwayTeam.Name == teamName && match.AwayTeamScore > match.HomeTeamScore));
+                standing.Lost = teamMatches.Count(match =>(match.HomeTeam.Name == teamName && match.HomeTeamScore < match.AwayTeamScore) || (match.AwayTeam.Name == teamName && match.AwayTeamScore < match.HomeTeamScore));
+                standing.Drawn = teamMatches.Count(match =>match.HomeTeamScore == match.AwayTeamScore);
+                standing.GoalsFavor = teamMatches.Sum(match =>match.HomeTeam.Name == teamName ? match.HomeTeamScore ?? 0 : match.AwayTeamScore ?? 0);
+                standing.GoalsAgainst = teamMatches.Sum(match =>match.HomeTeam.Name == teamName ? match.AwayTeamScore ?? 0 : match.HomeTeamScore ?? 0);
+                standing.GoalsDifference = standing.GoalsFavor - standing.GoalsAgainst;
+                standing.Points = (standing.Won * 3) + standing.Drawn;
+
+                standing.Form = CalculateForm(teamName, matches);
+            }
+
+            // Sort by points and goal difference
+            standings = standings.OrderByDescending(s => s.Points).ThenByDescending(s => s.GoalsDifference).ToList();
+
+            // Assign positions
+            for (var i = 0; i < standings.Count; i++)
+            {
+                standings[i].Position = i + 1;
+            }
 
             return View(standings);
         }
 
-        // GET: Users/Edit/5
-        public async Task<IActionResult> Edit(string id)
+        private string CalculateForm(string teamName, List<MatchSchedule> matches)
         {
-            return null;
-        }
-
-        // POST: Users/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string Id, string[] selectedRoles)
-        {
-            return null;
+           
+            return "WDL"; 
         }
 
 
