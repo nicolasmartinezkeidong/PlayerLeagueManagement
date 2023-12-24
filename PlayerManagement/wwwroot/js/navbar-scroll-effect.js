6feeedfceeda8f003513439d3885d44976dd5e85
NicolasKeidong <nicolasmartinezkeidong@outlook.com>
2023-12-23 21:46:00 -0500

    navbar final fixed

---
+++ PlayerManagement/wwwroot/js/navbar-scroll-effect.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(document).on('scroll', function () {
+        var $nav = $("#mainNavbar");
+        if ($(this).scrollTop() > 50) {
+            $nav.addClass('scrolled');
+        } else {
+            $nav.removeClass('scrolled');
+        }
+    });
+});
